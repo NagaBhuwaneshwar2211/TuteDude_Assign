3c93a3418bb25b82eb58320ce52e5e4427f8c20c
NagaBhuwaneshwar2211 <chittavadoota.naga@gmail.com>
2026-01-30 19:30:27 +0530

    assign1

---
+++ assign1.py
@@ -0,0 +1,28 @@
+#Task 1: Simple Calculator
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+print(f"Addition:", first_number + second_number)
+print(f"Subtraction:", first_number - second_number)
+print(f"Multiplication:", first_number * second_number)
+print(f"Division:", first_number / second_number)
+
+
+
+#Task 2: personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program")
+
+#Output:
+"""
+Enter the first number: 105
+Enter the second number: 12
+Addition: 117
+Subtraction: 93
+Multiplication: 1260
+Division: 8.75
+Enter your first name: Naga
+Enter your last name: bhuwaneshwar
+Hello, Naga bhuwaneshwar! Welcome to the Python program
+"""
